c0ca095b26b8e3422fb47f0f8b87ff0b283374de
raghavendrav3315 <raghavendrav3315@gmail.com>
2026-06-19 14:32:47 +0530

    tags added

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,7 +3,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "terrafrom-cicd-ec2" {
   ami           = var.ami_value
   instance_type = var.instance_type_value
+
+ tags = {
+    Name = "terrafrom-cicd-ec2"
+  }
 }
