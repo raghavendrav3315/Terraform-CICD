d60bc0730631c7cfd1867a5938ec1f850bb06c38
raghavendrav3315 <raghavendrav3315@gmail.com>
2026-06-19 13:44:32 +0530

    changed the indentantion in workflow file and added lockfile command to backend.tf

--- modules/ec2/backend.tf
+++ modules/ec2/backend.tf
@@ -1,7 +1,8 @@
 terraform{
     backend "s3" {
         bucket = "terraform-backend-for-cicd"
-        key    = "dev/terraform.tfstate"
+        key    = "ec2/terraform.tfstate"
         region = "us-east-1"
+        use_lockfile = true
 }
 }
